9ea6801ccfe2dda4e058297fe95a4c7194f89843
Eli Slothower <eli@slotho.io>
2021-11-29 16:28:24 -0500

    Added notes file

---
+++ notes.py
@@ -0,0 +1,7 @@
+#This file contains notes that I reference from time to time during development
+
+
+#THIS IS HOW YOU ACCESS ANIMAL INSTANCES!!!
+#This is necessary since animal instances are dynamic for the sim
+# for animalInstance in WolfNames:
+#     print(globals()[animalInstance].getMutationStatus())
