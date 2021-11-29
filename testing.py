8320b15488d220a3013d7ae265835110069fe4a0
Eli Slothower <eli@slotho.io>
2021-11-29 17:23:49 -0500

    Commented out all code

--- testing.py
+++ testing.py
@@ -2,28 +2,28 @@
 #Testing
 ######################################################################
 
-from matplotlib import pyplot as plt
-import numpy as np
+# from matplotlib import pyplot as plt
+# import numpy as np
 
-xValues = [0,1,2,3,4,5]
-yValues = [0,6,25,4,6,2]
+# xValues = [0,1,2,3,4,5]
+# yValues = [0,6,25,4,6,2]
 
-otherXValues = [0,12,15,20,24,28]
-otherYValues = [0,25, 25, 8, 1, 14]
+# otherXValues = [0,12,15,20,24,28]
+# otherYValues = [0,25, 25, 8, 1, 14]
 
-plt.scatter(xValues, yValues)
-plt.plot(xValues, yValues, 'red',
-        otherXValues, otherYValues, 'purple')
-
-
-
-plt.title("Sample Plot")
-plt.xlabel("X Label")
-plt.ylabel("Y Label")
-plt.axis([0,30,0,42]) #changes the axis points, x values go from 0-30, y values go from 0-42
+# plt.scatter(xValues, yValues)
+# plt.plot(xValues, yValues, 'red',
+#         otherXValues, otherYValues, 'purple')
 
 
-for i in range(4):
-    plt.plot(np.random.rand(10))
+
+# plt.title("Sample Plot")
+# plt.xlabel("X Label")
+# plt.ylabel("Y Label")
+# plt.axis([0,30,0,42]) #changes the axis points, x values go from 0-30, y values go from 0-42
+
+
+# for i in range(4):
+#     plt.plot(np.random.rand(10))
 
 #plt.show()
